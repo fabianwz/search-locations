6c9c705318220ceb342998c10f1d8e7f9dfa71ed
unknown <fgamboa@GAP-DSK-927.gap.com>
2016-08-01 10:27:11 -0600

    Added code to get last known location

--- app/src/main/java/com/searchlocations/SearchActivity.java
+++ app/src/main/java/com/searchlocations/SearchActivity.java
@@ -1,15 +1,29 @@
 package com.searchlocations;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class SearchActivity extends Activity {
+public class SearchActivity extends Activity implements GoogleApiClient.ConnectionCallbacks {
+
+    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
+    protected static final String TAG = "SearchActivity";
+
+    private GoogleApiClient apiClient;
 
     @BindView(R.id.editText) EditText textBox;
 
@@ -18,6 +32,57 @@ public class SearchActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_COARSE_LOCATION },
+                    PERMISSION_ACCESS_COARSE_LOCATION);
+        }
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Got permission!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (apiClient != null) {
+            apiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (apiClient.isConnected()) {
+            apiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "Connected to API", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "Connection suspended");
+        apiClient.connect();
     }
 
     @OnClick(R.id.searchButton)
@@ -25,7 +90,19 @@ public class SearchActivity extends Activity {
         String searchWord = textBox.getText().toString().trim();
         String text = (searchWord.isEmpty()) ? "Enter a value" : "Searching for " + searchWord;
         int duration = Toast.LENGTH_SHORT;
+        getLocation();
 
         Toast.makeText(this, text, duration).show();
+    }
+
+    private void getLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+            double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
+            String message = "Lat: " + lat + ", Long: " + lon;
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 }

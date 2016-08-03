a5b6d159f4f68843669547c904ff6d01ded04a2f
unknown <fgamboa@GAP-DSK-927.gap.com>
2016-08-03 11:26:01 -0600

    Added code to manage app errors

--- app/src/main/java/com/searchlocations/SearchActivity.java
+++ app/src/main/java/com/searchlocations/SearchActivity.java
@@ -5,22 +5,27 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class SearchActivity extends Activity implements GoogleApiClient.ConnectionCallbacks {
+public class SearchActivity extends Activity implements
+        ConnectionCallbacks, OnConnectionFailedListener {
 
-    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
+    private static final int PERMISSION_ACCESS_FINE_LOCATION = 1;
     protected static final String TAG = "SearchActivity";
 
     private GoogleApiClient apiClient;
@@ -33,10 +38,10 @@ public class SearchActivity extends Activity implements GoogleApiClient.Connecti
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[] { Manifest.permission.ACCESS_COARSE_LOCATION },
-                    PERMISSION_ACCESS_COARSE_LOCATION);
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    PERMISSION_ACCESS_FINE_LOCATION);
         }
         apiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -47,7 +52,7 @@ public class SearchActivity extends Activity implements GoogleApiClient.Connecti
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
-            case PERMISSION_ACCESS_COARSE_LOCATION:
+            case PERMISSION_ACCESS_FINE_LOCATION:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "Got permission!", Toast.LENGTH_SHORT).show();
                 } else {
@@ -85,24 +90,37 @@ public class SearchActivity extends Activity implements GoogleApiClient.Connecti
         apiClient.connect();
     }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Unable to connect to API", Toast.LENGTH_SHORT).show();
+    }
+
     @OnClick(R.id.searchButton)
     public void search() {
         String searchWord = textBox.getText().toString().trim();
-        String text = (searchWord.isEmpty()) ? "Enter a value" : "Searching for " + searchWord;
+        String text;
+        if (searchWord.isEmpty()) {
+           text = "Enter a value";
+        } else {
+            text = "Searching for " + searchWord;
+            getLocation();
+        }
         int duration = Toast.LENGTH_SHORT;
-        getLocation();
-
         Toast.makeText(this, text, duration).show();
     }
 
     private void getLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
 
-            double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
-            String message = "Lat: " + lat + ", Long: " + lon;
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            if(lastLocation != null) {
+                double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
+                String message = "Lat: " + lat + ", Long: " + lon;
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Unable to get location", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }

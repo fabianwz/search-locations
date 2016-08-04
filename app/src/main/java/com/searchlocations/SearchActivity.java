f9d96e64d4ec62324fb61be8bd98599950288eb8
unknown <fgamboa@GAP-DSK-927.gap.com>
2016-08-04 08:11:54 -0600

    Added location listener

--- app/src/main/java/com/searchlocations/SearchActivity.java
+++ app/src/main/java/com/searchlocations/SearchActivity.java
@@ -16,6 +16,9 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import butterknife.BindView;
@@ -23,12 +26,17 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SearchActivity extends Activity implements
-        ConnectionCallbacks, OnConnectionFailedListener {
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private static final int PERMISSION_ACCESS_FINE_LOCATION = 1;
     protected static final String TAG = "SearchActivity";
+    private static final int LOCATION_REFRESH_TIME = 10000;
 
-    private GoogleApiClient apiClient;
+    private FusedLocationProviderApi mProviderApi;
+
+    private GoogleApiClient mApiClient;
+
+    private String mText;
 
     @BindView(R.id.editText) EditText textBox;
 
@@ -37,45 +45,25 @@ public class SearchActivity extends Activity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
-                    PERMISSION_ACCESS_FINE_LOCATION);
-        }
-        apiClient = new GoogleApiClient.Builder(this)
+
+        mApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addApi(LocationServices.API)
                 .build();
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_ACCESS_FINE_LOCATION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Got permission!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
-                }
-
-                break;
-        }
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
-        if (apiClient != null) {
-            apiClient.connect();
+        if (mApiClient != null) {
+            mApiClient.connect();
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (apiClient.isConnected()) {
-            apiClient.disconnect();
+        if (mApiClient.isConnected()) {
+            mApiClient.disconnect();
         }
     }
 
@@ -87,7 +75,7 @@ public class SearchActivity extends Activity implements
     @Override
     public void onConnectionSuspended(int cause) {
         Log.i(TAG, "Connection suspended");
-        apiClient.connect();
+        mApiClient.connect();
     }
 
     @Override
@@ -97,30 +85,69 @@ public class SearchActivity extends Activity implements
 
     @OnClick(R.id.searchButton)
     public void search() {
-        String searchWord = textBox.getText().toString().trim();
+        mText = textBox.getText().toString().trim();
         String text;
-        if (searchWord.isEmpty()) {
+        if (mText.isEmpty()) {
            text = "Enter a value";
         } else {
-            text = "Searching for " + searchWord;
+            text = "Searching for " + mText;
             getLocation();
         }
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(this, text, duration).show();
     }
 
+
     private void getLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
 
-            if(lastLocation != null) {
-                double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
-                String message = "Lat: " + lat + ", Long: " + lon;
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(LOCATION_REFRESH_TIME);
+            locationRequest.setFastestInterval(LOCATION_REFRESH_TIME);
+
+            mProviderApi = LocationServices.FusedLocationApi;
+
+            mProviderApi.removeLocationUpdates(mApiClient, this);
+            mProviderApi.requestLocationUpdates(mApiClient, locationRequest, this);
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    PERMISSION_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_FINE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location != null) {
+            double lat = location.getLatitude(), lon = location.getLongitude();
+            String message = "Lat: " + lat + ", Long: " + lon;
+            if(mText != null){
+                //Busqueda pendiente
+                //Procesa busqueda
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Unable to get location", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Procesar texto", Toast.LENGTH_SHORT).show();
+                
+                mText = null;
             }
+
+        } else {
+            Toast.makeText(this, "Unable to get location", Toast.LENGTH_SHORT).show();
         }
     }
 }
